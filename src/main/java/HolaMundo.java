ef8184ed4b8f8433ab8844864c80350869620721
msmpunk <msm0102712@gmail.com>
2019-09-28 20:33:50 -0500

    + Parte 1

--- src/main/java/HolaMundo.java
+++ src/main/java/HolaMundo.java
@@ -1,19 +1,36 @@
 
 public class HolaMundo {
-
+    
+   // METODO PRINCIPAL
     public static void main(String[] args) {
+        var mensaje = "Hola Mundo";
+        var mensaje2 = "Soy Mario";
         
-        String saludar = "saludos desde Java";
+        HolaMundo Hola = new HolaMundo();
         
-        System.out.println(saludar);
-        System.out.println(saludar);
-        System.out.println(saludar);
+        // Primer Hola Mundo
+        System.out.println("Mi primer" + " " + mensaje );
         
-        var despedirse = "hasta luego";
-        System.out.println(despedirse);
+        // Segundo hola mundo
+        Hola.saludar(mensaje);
         
-        var numero = 1;
-        System.out.println(numero);
+        // Concatenar palabras
+        Hola.unirPalabras(mensaje, mensaje2);
+    }
+    
+    //METODO QUE SALUDA
+    public void saludar(String mensaje){
+        System.out.println("Este es un mensaje: " + mensaje );
+    }
+    
+    // METODO QUE CONCATENA DOS PALABRAS
+    public void unirPalabras(String palabra1, String palabra2){
         
+        // CARACTER ESPECIAL (\n) SALTO DE LINEA
+        System.out.println("Palabra 1: \n" + palabra1);
+        System.out.println("Palabra 2: \n" + palabra2 );
+        
+        // CARACTER ESPECIAL (\t) TABULACIÓN
+        System.out.println("Tu union es: \t" + palabra1 + " " + palabra2 );
     }
 }
